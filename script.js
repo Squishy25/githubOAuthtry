4b50218920a282d979b7c3c74c7becafb47306b4
Squishy25 <145614281+Squishy25@users.noreply.github.com>
2024-04-14 18:43:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function loginWithGitHub() {
     var clientID = '874bc1cc58cc2074d225';
-    var redirectURI = 'http://127.0.0.1:5500/redirect.html';
+    var redirectURI = 'https://squishy25.github.io/githubOAuthtry/redirect.html';
     var githubAuthURL = `https://github.com/login/oauth/authorize?client_id=${clientID}&redirect_uri=${redirectURI}&scope=user`;
 
     window.open(githubAuthURL, '_self');
